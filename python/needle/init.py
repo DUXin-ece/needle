36f7817c9f5ab4dff0acacb21e59bd81e994b792
Xin Du <xdu@ucsd.edu>
2022-12-15 07:35:57 -0800

    update initialization, nn.Conv

--- python/needle/init.py
+++ python/needle/init.py
@@ -4,15 +4,15 @@ import needle as ndl
 
 def rand(*shape, low=0.0, high=1.0, device=None, dtype="float32", requires_grad=False):
     """Generate random numbers uniform between low and high"""
-    device = ndl.cpu() if device is None else device
-    array = device.rand(*shape) * (high - low) + low
+    device = ndl.default_device() if device is None else device
+    array = device.rand(*shape, dtype=dtype) * (high - low) + low
     return ndl.Tensor(array, device=device, dtype=dtype, requires_grad=requires_grad)
 
 
 def randn(*shape, mean=0.0, std=1.0, device=None, dtype="float32", requires_grad=False):
     """Generate random normal with specified mean and std deviation"""
-    device = ndl.cpu() if device is None else device
-    array = device.randn(*shape) * std + mean
+    device = ndl.default_device() if device is None else device
+    array = device.randn(*shape, dtype=dtype) * std + mean
     return ndl.Tensor(array, device=device, dtype=dtype, requires_grad=requires_grad)
 
 
@@ -39,16 +39,16 @@ def zeros(*shape, device=None, dtype="float32", requires_grad=False):
 
 def randb(*shape, p=0.5, device=None, dtype="bool", requires_grad=False):
     """Generate binary random Tensor"""
-    device = ndl.cpu() if device is None else device
+    device = ndl.default_device() if device is None else device
     array = device.rand(*shape) <= p
     return ndl.Tensor(array, device=device, dtype=dtype, requires_grad=requires_grad)
 
 
 def one_hot(n, i, device=None, dtype="float32", requires_grad=False):
     """Generate one-hot encoding Tensor"""
-    device = ndl.cpu() if device is None else device
+    device = ndl.default_device() if device is None else device
     return ndl.Tensor(
-        device.one_hot(n, i.numpy(), dtype=dtype),
+        device.one_hot(n, i.numpy().astype("int32"), dtype=dtype),
         device=device,
         requires_grad=requires_grad,
     )
@@ -68,33 +68,45 @@ def ones_like(array, *, device=None, requires_grad=False):
     )
 
 
-def xavier_uniform(fan_in, fan_out, gain=1.0, **kwargs):
+def xavier_uniform(fan_in, fan_out, shape=None, gain=1.0, **kwargs):
     ### BEGIN YOUR SOLUTION
     a = gain * math.sqrt(6 / (fan_in + fan_out))
-    return rand(fan_in, fan_out, low=-a, high=a)
+    if shape == None:
+        return rand(fan_in, fan_out, low=-a, high=a)
+    else:
+        return rand(*shape, low=-a, high=a)
     ### END YOUR SOLUTION
 
 
-def xavier_normal(fan_in, fan_out, gain=1.0, **kwargs):
+def xavier_normal(fan_in, fan_out, shape=None, gain=1.0, **kwargs):
     ### BEGIN YOUR SOLUTION
     std = gain * math.sqrt(2 / (fan_in + fan_out))
-    return randn(fan_in, fan_out, std=std)
+    if shape == None:
+        return randn(fan_in, fan_out, std=std)
+    else:
+        return randn(*shape, std=std)
     ### END YOUR SOLUTION
 
 
-def kaiming_uniform(fan_in, fan_out, nonlinearity="relu", **kwargs):
+def kaiming_uniform(fan_in, fan_out, shape=None, nonlinearity="relu", **kwargs):
     assert nonlinearity == "relu", "Only relu supported currently"
     ### BEGIN YOUR SOLUTION
     gain = math.sqrt(2)
     bound = gain * math.sqrt(3 / fan_in)
-    return rand(fan_in, fan_out, low=-bound, high=bound)
+    if shape == None:
+        return rand(fan_in, fan_out, low=-bound, high=bound)
+    else:
+        return rand(*shape, low=-bound, high=bound)
     ### END YOUR SOLUTION
 
 
-def kaiming_normal(fan_in, fan_out, nonlinearity="relu", **kwargs):
+def kaiming_normal(fan_in, fan_out, shape=None, nonlinearity="relu", **kwargs):
     assert nonlinearity == "relu", "Only relu supported currently"
     ### BEGIN YOUR SOLUTION
     gain = math.sqrt(2)
     std = gain / math.sqrt(fan_in)
-    return randn(fan_in, fan_out, std=std)
+    if shape == None:
+        return randn(fan_in, fan_out, std=std)
+    else:
+        return randn(*shape, std=std)
     ### END YOUR SOLUTION
